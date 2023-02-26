d3efb684fcb51895831fa5e18c3ffe97f825b237
Stefan Boos <kontakt@boos.systems>
2023-02-26 21:07:07 +0100

    feat: target directory can be specified

--- MarkdownLinkedImagesMover.Tests/ProgramTests.cs
+++ MarkdownLinkedImagesMover.Tests/ProgramTests.cs
@@ -11,10 +11,13 @@ public class ProgramTests
         var writer = new StringWriter();
         Console.SetOut(writer);
 
-        Program.Main(new FileInfo("data/source/Testfile.md"));
+        Program.Main(new FileInfo("data/source/Testfile.md"), new DirectoryInfo("data/target"));
 
         var output = writer.ToString();
-        Assert.StartsWith("File 'Testfile.md' contains", output);
+
+        Assert.Matches("Target folder: '.*/data/target'", output);
+
+        Assert.Contains("File 'Testfile.md' contains", output);
         Assert.Contains("'noun-starship-3799189.png'", output);
         Assert.Contains("'noun-island-1479438.png'", output);
     }
