95a8d6cffa06851b0d1a4ecf95ead12ebacd656b
Stefan Boos <kontakt@boos.systems>
2023-02-28 21:16:41 +0100

    test: run filesystem tests in parallel
    TestDirectory allows to have multiple data directories
    for multiple tests running in parallel.

--- MarkdownLinkedImagesMover.Tests/FileMoverTests.cs
+++ MarkdownLinkedImagesMover.Tests/FileMoverTests.cs
@@ -1,23 +1,39 @@
+using System;
 using System.IO;
 
 namespace MarkdownLinkedImagesMover.Tests;
 
-public class FileMoverTests
+public sealed class FileMoverTests : IDisposable
 {
-    public FileMoverTests()
-    {
-        TestData.Setup();
-    }
+    private readonly TestDirectory _testDir;
+
+    public FileMoverTests() => _testDir = TestDirectory.Create();
 
     [Fact]
     public void MoveExistingFileToValidDestination()
     {
-        var sourceFile = new FileInfo("data/source/Testfile.md");
-        var targetDir = new DirectoryInfo("data/target");
+        var sourceFile = new FileInfo(Path.Combine(_testDir.SourceDir.FullName, "Testfile.md"));
+        var targetDir = _testDir.TargetDir;
 
         FileMover.Move(sourceFile, targetDir);
 
         var hasMoved = new FileInfo(Path.Combine(targetDir.FullName, sourceFile.Name)).Exists;
         Assert.True(hasMoved, "file should be moved to target folder");
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        _testDir.Delete();
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    ~FileMoverTests()
+    {
+        ReleaseUnmanagedResources();
     }
 }
---
+++ MarkdownLinkedImagesMover.Tests/TestDirectory.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace MarkdownLinkedImagesMover.Tests;
+
+public class TestDirectory
+{
+    private DirectoryInfo _baseDir;
+
+    public DirectoryInfo SourceDir { get; private set; }
+
+    public DirectoryInfo TargetDir { get; private set; }
+
+    public static TestDirectory Create()
+    {
+        var result = new TestDirectory();
+        result.CreateDirectories();
+        result.CopySeedToSource();
+        return result;
+    }
+
+    private void CreateDirectories()
+    {
+        var dateString = DateTime.Now.ToString("yyyyMMddTHHmmss", CultureInfo.InvariantCulture);
+        var uid = Guid.NewGuid();
+        _baseDir = new DirectoryInfo($"data/test-{dateString}-{uid}");
+        _baseDir.Create();
+
+        SourceDir = new DirectoryInfo(Path.Combine(_baseDir.FullName, "source"));
+        SourceDir.Create();
+
+        TargetDir = new DirectoryInfo(Path.Combine(_baseDir.FullName, "target"));
+        TargetDir.Create();
+    }
+
+    private void CopySeedToSource()
+    {
+        var seedDir = new DirectoryInfo("data/seed");
+        foreach (var seedFile in seedDir.GetFiles())
+        {
+            var targetFile = Path.Combine(SourceDir.FullName, seedFile.Name);
+            seedFile.CopyTo(targetFile);
+        }
+    }
+
+    public void Delete()
+    {
+        _baseDir.Delete(recursive: true);
+    }
+}
