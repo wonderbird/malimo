a94d6cd34da3a89367864d4d951833b84013a860
Stefan Boos <kontakt@boos.systems>
2023-03-04 23:01:54 +0100

    feat: log messages show immediately on single line

---
+++ MarkdownLinkedImagesMover/SingleLineConsoleFormatter.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+
+namespace MarkdownLinkedImagesMover;
+
+internal sealed class SingleLineConsoleFormatter : ConsoleFormatter
+{
+    public SingleLineConsoleFormatter(IOptionsMonitor<ConsoleFormatterOptions> _)
+        : base(nameof(SingleLineConsoleFormatter))
+    {
+    }
+
+    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider scopeProvider, TextWriter textWriter)
+    {
+        var message = logEntry.Formatter.Invoke(logEntry.State, logEntry.Exception);
+        textWriter.WriteLine($"{logEntry.LogLevel}: {message}");
+    }
+}
