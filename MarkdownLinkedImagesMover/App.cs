1860bc87baf6d4d66db547a55faf14357e3deb86
Stefan Boos <kontakt@boos.systems>
2023-03-05 21:11:16 +0100

    refactor: extract methods for each aspect from App.Run

--- MarkdownLinkedImagesMover/App.cs
+++ MarkdownLinkedImagesMover/App.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.Logging;
@@ -12,16 +13,27 @@ internal class App
 
     public void Run(FileInfo markdownFile, DirectoryInfo targetDir)
     {
-        var fileContent = File.ReadAllText(markdownFile.FullName);
-        var imageNames = MarkdownParser.ParseLinkedImages(fileContent).ToList();
+        var imageNames = GetImagesFromMarkdownFile(markdownFile);
+        LogImageNames(markdownFile, targetDir, imageNames);
+        MoveImagesToTargetDir(markdownFile, targetDir, imageNames);
+    }
 
+    private static List<string> GetImagesFromMarkdownFile(FileSystemInfo markdownFile)
+    {
+        var fileContent = File.ReadAllText(markdownFile.FullName);
+        return MarkdownParser.ParseLinkedImages(fileContent).ToList();
+    }
+
+    private void LogImageNames(FileSystemInfo markdownFile, FileSystemInfo targetDir, List<string> imageNames)
+    {
         Logger.LogInformation("Target folder: '{@TargetFolder}'", targetDir.FullName);
         Logger.LogInformation("File '{@SourceFile}' contains", markdownFile.FullName);
-        imageNames.ToList().ForEach(imageName => Logger.LogInformation("- '{@ImageFile}'", imageName));
+        imageNames.ForEach(imageName => Logger.LogInformation("- '{@ImageFile}'", imageName));
+    }
 
+    private static void MoveImagesToTargetDir(FileInfo markdownFile, DirectoryInfo targetDir, IEnumerable<string> imageNames) =>
         imageNames
-            .Select(imageName => new FileInfo(Path.Combine(markdownFile.DirectoryName?? "", imageName)))
+            .Select(imageName => new FileInfo(Path.Combine(markdownFile.DirectoryName ?? "", imageName)))
             .ToList()
             .ForEach(sourceFile => FileMover.Move(sourceFile, targetDir));
-    }
 }
