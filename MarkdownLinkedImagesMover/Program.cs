6c8edf4fbe63e1eb34ff60c925336665519a6501
Stefan Boos <kontakt@boos.systems>
2023-03-09 20:17:33 +0100

    feat: the parameter --no-action allows to simulate without moving

--- MarkdownLinkedImagesMover/Program.cs
+++ MarkdownLinkedImagesMover/Program.cs
@@ -1,8 +1,5 @@
 using System.IO;
-using System.IO.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 
 namespace MarkdownLinkedImagesMover;
 
@@ -12,38 +9,15 @@ namespace MarkdownLinkedImagesMover;
 public static class Program
 {
     /// <summary>
-    /// Move all images referred to by the given markdown file to a folder.
+    /// Move all images used by a markdown file to a folder.
     /// </summary>
     /// <param name="file">Markdown file containing the references to image files which shall be moved</param>
     /// <param name="targetDir">Move files to this target folder</param>
-    public static void Main(FileInfo file, DirectoryInfo targetDir)
+    /// <param name="noAction">Do not move the files (dry run)</param>
+    public static void Main(FileInfo file, DirectoryInfo targetDir, bool noAction = false)
     {
-        var serviceCollection = new ServiceCollection();
-        ConfigureServices(serviceCollection);
-        var serviceProvider = serviceCollection.BuildServiceProvider();
-
+        var serviceProvider = ServiceProviderFactory.CreateServiceProvider(noAction);
         var app = serviceProvider.GetRequiredService<App>();
         app.Run(file, targetDir);
     }
-
-    private static void ConfigureServices(IServiceCollection serviceCollection) =>
-        serviceCollection
-            .AddLoggingToConsoleAndDebug()
-            .AddTransient<IFileSystem, FileSystem>()
-            .AddTransient<IFileMover, FileMover>()
-            .AddSingleton<App>();
-
-    private static IServiceCollection AddLoggingToConsoleAndDebug(this IServiceCollection serviceCollection) =>
-        serviceCollection.AddLogging(configure =>
-        {
-            configure.SetMinimumLevel(LogLevel.Debug);
-            configure.AddDebug();
-            configure.AddConsole(options =>
-            {
-                // Show all log messages immediately
-                options.MaxQueueLength = 1;
-                options.FormatterName = nameof(SingleLineConsoleFormatter);
-            });
-            configure.AddConsoleFormatter<SingleLineConsoleFormatter, ConsoleFormatterOptions>();
-        });
 }
