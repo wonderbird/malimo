8e73573e684d450b2a2caac63d14485259023538
Stefan Boos <kontakt@boos.systems>
2023-04-12 11:38:55 +0200

    refactor: rename BuildAndReleaseScripts directory to build

--- build/SignMac.sh
+++ build/SignMac.sh
@@ -2,11 +2,11 @@
 # This file is adopted from https://github.com/coding-flamingo/CodeSignConsoleApp/blob/main/BuildAndReleaseScripts/SignMac.sh
 # and from https://www.kenmuse.com/blog/notarizing-dotnet-console-apps-for-macos/ 
 #
-# usage /bin/sh BuildAndReleaseScripts/SignMac.sh "markdown-linked-images-mover/MarkdownLinkedImagesMover" "Developer ID Application: Stefan Boos" "BuildAndReleaseScripts/entitlements.plist"
+# usage /bin/sh ./build/SignMac.sh "markdown-linked-images-mover/MarkdownLinkedImagesMover" "Developer ID Application: Stefan Boos" "./build/entitlements.plist"
 #
 # RunFile      $1 "markdown-linked-images-mover/MarkdownLinkedImagesMover"
 # CertName     $2 "Developer ID Application: YOURORG (YOURDEVID)" 
-# Entitlements $3 "BuildAndReleaseScripts/entitlements.plist"
+# Entitlements $3 "./build/entitlements.plist"
 
 echo "======== INPUTS ========"
 echo "RunFile: $1"
