1aef8160eb1abf6a1f4116c8288f465777a32ce1
Stefan Boos <kontakt@boos.systems>
2023-04-19 02:56:57 +0200

    feat: --no-action logs what would be done

--- malimo/NoAction.cs
+++ malimo/NoAction.cs
@@ -1,8 +1,17 @@
 using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace malimo;
 
 internal class NoAction : IFileMover
 {
-    public void Move(FileInfo sourceFile, DirectoryInfo targetDir) { }
+    private readonly ILogger<NoAction> _logger;
+
+    public NoAction(ILogger<NoAction> logger) => _logger = logger;
+
+    public void Move(FileInfo sourceFile, DirectoryInfo targetDir)
+    {
+        // TODO: Add Test
+        _logger.LogInformation("Would move '{@SourceFile}' to '{@TargetFolder}'", sourceFile.FullName, targetDir.FullName);
+    }
 }
