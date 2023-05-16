ad013d8257c4dfe354c279b2e906172d291163f2
Stefan Boos <kontakt@boos.systems>
2023-05-16 05:30:32 +0200

    test: end-to-end test verifies log messages

--- malimo.Integration.Tests/ProgramTests.cs
+++ malimo.Integration.Tests/ProgramTests.cs
@@ -5,11 +5,23 @@ public class ProgramTests
     [Fact]
     public void VerboseDryRun()
     {
+        using var testDir = TestDirectory.Create();
+
         var process = new TestProcessWrapper.TestProcessWrapper("malimo", false);
+        var sourceFile = new FileInfo(Path.Combine(testDir.SourceDir.FullName, "Testfile.md"));
+        var targetDir = testDir.TargetDir;
+
+        process.AddCommandLineArgument("--file", sourceFile.FullName);
+        process.AddCommandLineArgument("--target-dir", targetDir.FullName);
+        process.AddCommandLineArgument("--dry-run");
+
         process.Start();
         process.WaitForProcessExit();
+        var hasProcessExitedGracefully = process.HasExited;
+
         process.ForceTermination();
 
         Assert.Matches("Would move '.*/noun-island-1479438.png'", process.RecordedOutput);
+        Assert.True(hasProcessExitedGracefully, "malimo should exit gracefully");
     }
 }
