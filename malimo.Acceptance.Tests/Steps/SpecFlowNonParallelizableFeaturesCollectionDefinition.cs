701a77ec4763a5ab23037560b3f16043ffb1b6b5
Stefan Boos <kontakt@boos.systems>
2023-05-20 22:36:10 +0200

    test: force specflow tests executed sequentially

---
+++ malimo.Acceptance.Tests/Steps/SpecFlowNonParallelizableFeaturesCollectionDefinition.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace malimo.Acceptance.Tests.Steps;
+
+[CollectionDefinition("SpecFlowNonParallelizableFeatures", DisableParallelization = true)]
+public class SpecFlowNonParallelizableFeaturesCollectionDefinition
+{
+}
